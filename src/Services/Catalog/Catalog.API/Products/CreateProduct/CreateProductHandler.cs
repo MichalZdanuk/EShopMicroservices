020cc97d9371e4a2d8fcb47b4393aea5b54adbc0
MichalZdanuk <zdanuk_m@wp.pl>
2025-02-17 20:43:52 +0100

    Implement connection with PostgresDB, prepare docker-compose.

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,13 +1,10 @@
-﻿using BuildingBlocks.CQRS;
-using Catalog.API.Models;
-
-namespace Catalog.API.Products.CreateProduct;
+﻿namespace Catalog.API.Products.CreateProduct;
 
 public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
 	: ICommand<CreateProductResult>;
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler
+internal class CreateProductCommandHandler(IDocumentSession session)
 	: ICommandHandler<CreateProductCommand, CreateProductResult>
 {
 	public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
@@ -21,8 +18,10 @@ internal class CreateProductCommandHandler
 			Price = command.Price,
 		};
 
-		// save to db
+		session.Store(product);
 
-		return new CreateProductResult(Guid.NewGuid());
+		await session.SaveChangesAsync(cancellationToken);
+
+		return new CreateProductResult(product.Id);
 	}
 }
