7e9122d376cd66ef310647033c972cbf07c21eb3
MichalZdanuk <zdanuk_m@wp.pl>
2025-02-19 15:01:00 +0100

    Implement LoggingBehavior, apply it to pipeline and remove manual logging.

--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductHandler.cs
@@ -4,14 +4,11 @@ namespace Catalog.API.Products.GetProducts;
 public record GetProductsQuery() : IQuery<GetProductsResult>;
 public record GetProductsResult(IEnumerable<Product> Products);
 
-internal class GetProductQueryHandler(IDocumentSession session,
-	ILogger<GetProductQueryHandler> logger)
+internal class GetProductQueryHandler(IDocumentSession session)
 	: IQueryHandler<GetProductsQuery, GetProductsResult>
 {
 	public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
 	{
-		logger.LogInformation("GetProductQueryHandler.Handle called with {@query}", query);
-
 		var products = await session.Query<Product>().ToListAsync(cancellationToken);
 
 		return new GetProductsResult(products);
