b8493fb271a09c9b52892b8a04ecc29990caa140
MichalZdanuk <zdanuk_m@wp.pl>
2025-02-19 20:34:00 +0100

    Implement naive StoreBasket feature.

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,5 +1,27 @@
 ﻿namespace Basket.API.Basket.StoreBasket;
 
-public class StoreBasketHandler
+public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+public record StoreBasketResult(string UserName);
+
+public class StoreBasketValidator : AbstractValidator<StoreBasketCommand>
 {
+	public StoreBasketValidator()
+	{
+		RuleFor(x => x.Cart).NotEmpty().WithMessage("Cart can not be null");
+		RuleFor(x => x.Cart.UserName).NotEmpty().WithMessage("UserName is required");
+	}
+}
+
+internal class StoreBasketCommandHandler
+	: ICommandHandler<StoreBasketCommand, StoreBasketResult>
+{
+	public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
+	{
+		ShoppingCart cart = command.Cart;
+
+		// TODO: store basket in DB (use Marten upsert)
+		// TODO: update cache
+
+		return new StoreBasketResult("abc");
+	}
 }
