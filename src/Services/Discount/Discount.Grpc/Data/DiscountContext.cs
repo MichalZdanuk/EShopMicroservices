6bff1d3f840c32a1cf2c41711645b69666e9823b
MichalZdanuk <zdanuk_m@wp.pl>
2025-02-23 18:39:34 +0100

    Prepare InitialDb migration and generate db.

--- src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
+++ src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -5,7 +5,7 @@ namespace Discount.Grpc.Data;
 
 public class DiscountContext : DbContext
 {
-	public DbSet<Coupon> Couponse { get; set; } = default!;
+	public DbSet<Coupon> Coupons { get; set; } = default!;
 	public DiscountContext(DbContextOptions<DiscountContext> options)
 		: base(options)
 	{
