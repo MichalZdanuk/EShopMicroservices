0e1cb8e53f4c996ac2f1e71fba97f2c99035e8d9
MichalZdanuk <zdanuk_m@wp.pl>
2025-03-01 11:44:29 +0100

    Refactor code to extract mapping order to orderDto out to extension method.

--- src/Services/Ordering/Ordering.Application/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -5,3 +5,4 @@ global using Ordering.Application.Data;
 global using Ordering.Application.Dtos;
 global using Ordering.Application.Exceptions;
 global using Ordering.Domain.Events;
+global using Microsoft.EntityFrameworkCore;
