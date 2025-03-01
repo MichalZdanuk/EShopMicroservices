ef0b1a8964a36d83683c69972fc0cb469ada91cd
MichalZdanuk <zdanuk_m@wp.pl>
2025-03-01 11:51:10 +0100

    Implement GetOrdersByCustomer logic.

--- src/Services/Ordering/Ordering.Application/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -6,3 +6,4 @@ global using Ordering.Application.Dtos;
 global using Ordering.Application.Exceptions;
 global using Ordering.Domain.Events;
 global using Microsoft.EntityFrameworkCore;
+global using Ordering.Application.Extensions;
