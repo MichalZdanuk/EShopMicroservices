1bfac2073652eef73b600ef0f9e6f6d8ff603cd6
MichalZdanuk <zdanuk_m@wp.pl>
2025-03-02 20:24:47 +0100

    Register RabbitMQ message broker in Ordering microservice.

--- src/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -1,11 +1,13 @@
 ﻿using BuildingBlocks.Behaviors;
+using BuildingBlocks.Messaging.MassTransit;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
 namespace Ordering.Application;
 public static class DependencyInjection
 {
-	public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+	public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
 	{
 		services.AddMediatR(config =>
 		{
@@ -13,6 +15,8 @@ public static class DependencyInjection
 			config.AddOpenBehavior(typeof(ValidationBehavior<,>));
 			config.AddOpenBehavior(typeof(LoggingBehavior<,>));
 		});
+
+		services.AddMessageBroker(configuration, Assembly.GetExecutingAssembly());
 
 		return services;
 	}
