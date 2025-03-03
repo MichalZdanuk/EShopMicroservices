04e29777bd9426106c9812573569e1888e35a6dd
MichalZdanuk <zdanuk_m@wp.pl>
2025-03-03 20:50:42 +0100

    Split EventHandlers into Domain and Integration in Ordering.Application.

--- src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
@@ -1,7 +1,7 @@
 ﻿using MediatR;
 using Microsoft.Extensions.Logging;
 
-namespace Ordering.Application.Orders.EventHandlers;
+namespace Ordering.Application.Orders.EventHandlers.Domain;
 public class OrderCreatedEventHandler(ILogger<OrderCreatedEventHandler> logger)
 	: INotificationHandler<OrderCreatedEvent>
 {
